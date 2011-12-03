9c3fc4b40b719f9bb79f4f8385f65f6d112329d6
Aeshan Wijetunge <aeshanw@gmail.com>
2011-12-04 01:47:51 +0800

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hucklebury::Application.config.secret_token = 'e9fd6214cf03813c4b447882e41e7cec81dc2adb1a0f6780c6f4b325f2b443cae3ef6a493f830618a28bb54d6bfde238aa0c14839dfde7016db05e8db08e61ec'
