0940182808931ea08baddc1549cb0adc3fa7d89f
Dhawal Shah <dhawaljshah@gmail.com>
2011-12-04 02:05:21 +0800

    Created Landing Page as root

---
+++ app/controllers/landing_controller.rb
@@ -0,0 +1,6 @@
+class LandingController < Spree::BaseController
+  layout 'spree_application'
+  def index
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Hucklebury::Application.routes.draw do
+  get "landing/index"
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -12,6 +14,7 @@ Hucklebury::Application.routes.draw do
 
   # Sample resource route (maps HTTP verbs to controller actions automatically):
   #   resources :products
+  root :to => "landing#index"
 
   # Sample resource route with options:
   #   resources :products do
