9aed86cc388c64ea7fc9fdbb6dcf7fc24b4d11f4
Dhawal Shah <dhawaljshah@gmail.com>
2011-12-26 15:00:01 +0800

    removed assets from app that were moved to lib or vendor

--- app/overrides/add_email_to_friend_to_product_page.rb
+++
@@ -1,4 +0,0 @@
-Deface::Override.new(:virtual_path => "products/show",
-                     :name => "add_email_to_friend",
-                     :insert_after => "[data-hook='cart_form'], div#cart-form",
-                     :partial => "products/email_link")
--- app/overrides/remove_email_to_friend_to_product_page.rb
+++
@@ -1,3 +0,0 @@
-Deface::Override.new(:virtual_path => "products/show",
-                     :name => "remove_email_to_friend",
-                     :remove => "p.email_to_friend")
